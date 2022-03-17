87f44e76b55b1131d4fa10c1984e5587bfbea987
Rafael Martinez <rafael@megsoftconsulting.com>
2022-03-17 15:21:18 -0600

    Adding Android Notification Hub.

--- Finance.Android/MainActivity.cs
+++ Finance.Android/MainActivity.cs
@@ -6,18 +6,29 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using WindowsAzure.Messaging.NotificationHubs;
+using Finance.Droid.Overrides;
 
 namespace Finance.Droid
 {
     [Activity(Label = "Finance", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        internal static readonly string CHANNEL_ID = "my_notification_channel";
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
+
+            // Listen for push notifications
+            //NotificationHub.SetListener(new AzureListener());
+
+            // Start the SDK
+            //NotificationHub.Start(this.Application, Constants.NotificationHubName, Constants.ListenConnectionString);
+
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
