b9faf20913e620105ffac8b94ab4728b1ee086fb
Rafael Martinez <rafael@megsoftconsulting.com>
2022-03-17 14:30:51 -0600

    iOS Push Notification - Firebase.

--- Finance/View/MainPage.xaml.cs
+++ Finance/View/MainPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Plugin.FirebasePushNotification;
 using Xamarin.Forms;
 
 namespace Finance.View
@@ -11,6 +11,14 @@ namespace Finance.View
         {
             InitializeComponent();
             Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
+
+            CrossFirebasePushNotification.Current.OnNotificationReceived += Current_OnNotificationReceived;
+
+        }
+
+        private void Current_OnNotificationReceived(object source, FirebasePushNotificationDataEventArgs e)
+        {
+            DisplayAlert("Notification", $"Data: {e.Data["myData"]}", "OK");
         }
     }
 }
