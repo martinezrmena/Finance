5730506a38ef528b92ccf97bfa2f2c581c77d7d1
Rafael Martinez <rafael@megsoftconsulting.com>
2022-03-16 19:21:40 -0600

    Adding Push notifications

--- Finance/App.xaml.cs
+++ Finance/App.xaml.cs
@@ -2,6 +2,9 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Finance.View;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Analytics;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace Finance
@@ -15,9 +18,18 @@ namespace Finance
             MainPage = new NavigationPage(new MainPage());
         }
 
-        protected override void OnStart()
+        protected async override void OnStart()
         {
-            // Handle when your app starts
+            string androidAppSecret = "1f5e18b3-b721-43a7-b766-21d92e54bd2f";
+            string iOSAppSecret = "ebc8b9c4-8c22-44ff-8591-fef184f4d2ab";
+            AppCenter.Start($"android={androidAppSecret};ios={iOSAppSecret}", typeof(Crashes), typeof(Analytics));
+
+            var didAppCrashed = await Crashes.HasCrashedInLastSessionAsync();
+
+            if (didAppCrashed)
+            {
+                var crashReport = await Crashes.GetLastSessionCrashReportAsync();
+            }
         }
 
         protected override void OnSleep()
--- Finance/View/PostPage.xaml.cs
+++ Finance/View/PostPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using Finance.Model;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using Xamarin.Forms;
 
 namespace Finance.View
@@ -16,9 +18,40 @@ namespace Finance.View
         public PostPage(Item item)
         {
             InitializeComponent();
-            Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
 
-            webView.Source = "https://stackoverflow.com/questions/52793957/xamarin-forms-display-image-from-embedded-resource-using-xaml";
+            try
+            {
+                Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
+
+                webView.Source = "https://stackoverflow.com/questions/52793957/xamarin-forms-display-image-from-embedded-resource-using-xaml";
+                //throw (new Exception("Unable to log blog"));
+
+                var properties = new Dictionary<string, string>()
+                {
+                    {"Blog post", $"{item.Title}"}
+                };
+                TrackEvent(properties);
+            }
+            catch (Exception ex)
+            {
+                var properties = new Dictionary<string, string>()
+                {
+                    {"Blog post", $"{item.Title}"}
+                };
+                TrackError(ex, properties);
+            }
+        }
+
+        private async void TrackEvent(Dictionary<string, string> properties)
+        {
+            if (await Analytics.IsEnabledAsync())
+                Analytics.TrackEvent("Blog post opened", properties);
+        }
+
+        private async void TrackError(Exception ex, Dictionary<string, string> properties)
+        {
+            if (await Crashes.IsEnabledAsync())
+                Crashes.TrackError(ex, properties);
         }
     }
 }
