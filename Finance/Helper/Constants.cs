106676ecf01d6685707cb68fd3d3e60d918d0b15
Rafael Martinez <rafael@megsoftconsulting.com>
2022-03-23 16:44:15 -0600

    adding missing file

---
+++ Finance/Helper/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Finance.Helper
+{
+    public static class Constants
+    {
+        public static string ListenConnectionString = "Endpoint=sb://financehR.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=EnnB2FsuvcuGk1kIJv/xPf1nXk1vrqXnWWkyziLj07Q=";
+        public static string NotificationHubName = "FinanceNotificationHub";
+    }
+}
---
+++ Finance/Helper/WebDirections.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Finance.Helper
+{
+    public static class WebDirections
+    {
+        public static string AzurePortal = "https://portal.azure.com/?quickstart=true#@rafaelalfonhotmail.onmicrosoft.com/resource/subscriptions/22fb68c8-775a-48f2-a71f-70cf9a8c020b/resourcegroups/FinanceNotification/providers/Microsoft.NotificationHubs/namespaces/financehR/notificationHubs/FinanceNotificationHub/accessPolicies";
+
+        public static string FireBaseSetUp = "https://docs.microsoft.com/en-us/azure/notification-hubs/xamarin-notification-hubs-push-notifications-android-gcm#registering-with-firebase-cloud-messaging";
+
+
+    }
+}
